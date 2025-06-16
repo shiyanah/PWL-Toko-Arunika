29b0c264b76cb88e7ef17cc5e16a46edbeb31f92
Shiyanah <shiyanah68@gmail.com>
2025-06-16 13:27:26 +0700

    first commit

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -5,9 +5,29 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Home::index');
-$routes->get('/produk', 'Home::produk');
-$routes->get('/kategori/(:num)', 'Home::kategori/$1');
-$routes->get('/kategori', 'Home::kategori');
+
+$routes->get('/', 'Home::index', ['filter' => 'auth']);
+
+$routes->get('/user-profile', 'Home::profile');
+$routes->get('/page-faq', 'Home::faq');
+
+$routes->get('login', 'AuthController::login');
+$routes->post('login', 'AuthController::login');
+$routes->get('logout', 'AuthController::logout');
+
+$routes->group('produk', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'ProductController::index');
+    $routes->post('', 'ProductController::create');
+    $routes->post('edit/(:any)', 'ProductController::edit/$1');
+    $routes->get('delete/(:any)', 'ProductController::delete/$1');
+    $routes->get('download', 'ProductController::download');
+});
 
 
+$routes->group('keranjang', ['filter' => 'auth'], function ($routes) {
+    $routes->get('', 'TransaksiController::index');
+    $routes->post('', 'TransaksiController::cart_add');
+    $routes->post('edit', 'TransaksiController::cart_edit');
+    $routes->get('delete/(:any)', 'TransaksiController::cart_delete/$1');
+    $routes->get('clear', 'TransaksiController::cart_clear');
+});
